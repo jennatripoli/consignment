e500897e776afe4b9e51802268c4429f23d83060
Jacob <jrmadams02@gmail.com>
2023-10-24 19:23:05 -0400

    removed mqp stuff :)

--- providers.tf
+++ providers.tf
@@ -5,14 +5,6 @@ terraform {
       version = "5.16.2"
     }
   }
-  # backend "s3" {
-  #   bucket         = "mqp-terraform-state-bucket"
-  #   key            = "frontend/terraform.tfstate"
-  #   region         = "us-east-1"
-  #   dynamodb_table = "mqp-terraform-state-lock-table"
-  #   encrypt        = true
-  #   profile        = "509"
-  # }
 }
 
 provider "aws" {
