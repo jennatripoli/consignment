bcb662a6ad6114c4151d80c55cdf11b5f6f920a1
Jacob <jrmadams02@gmail.com>
2023-10-24 18:04:27 -0400

    fixed create store button

--- src/OwnerCreateStore.js
+++ src/OwnerCreateStore.js
@@ -33,7 +33,7 @@ export default function OwnerCreateStore() {
       const data = { 'body': JSON.stringify(msg) }
 
       // FOR TESTING
-      setCurrentPageName('OwnerViewStore')
+      navigate('/OwnerViewStore')
 
       /*instance.post('/ownerCreateStore', data).then((response) => {
         if (response.status === 200) {
