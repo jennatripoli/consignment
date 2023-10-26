802b0ef5c73618eaedd3317db553d5f590a07e1e
Jacob <jrmadams02@gmail.com>
2023-10-26 00:44:59 -0400

    added store lambda

---
+++ lambdas/StoresLambda/lambda.py
@@ -0,0 +1,43 @@
+import os
+import psycopg2
+
+def lambda_handler(event, context):
+    # Retrieve database connection parameters from environment variables
+    db_params = {
+        'user': os.environ['DB_USER'],
+        'password': os.environ['DB_PASSWORD'],
+        'host': os.environ['DB_HOST'],
+        'database': os.environ['DB_NAME'],
+        'port': 5432  # Default PostgreSQL port
+    }
+
+    # Establish a connection to the PostgreSQL database
+    conn = psycopg2.connect(**db_params)
+    result = ''
+    try:
+        # Create a cursor object
+        cursor = conn.cursor()
+
+        # Execute your SQL queries here
+        cursor.execute("SELECT * FROM store")
+
+        # Fetch and process the query results as needed
+        result = cursor.fetchall()
+
+    except Exception as e:
+        return {
+        'statusCode': 500,
+        'body': 'something went wrong'
+    }
+    finally:
+        # Close the cursor and database connection
+        cursor.close()
+        conn.close()
+
+    return {
+        'statusCode': 200,
+        'body': result
+    }
+
+
+lambda_handler('','')
