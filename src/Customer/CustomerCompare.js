8ea263a259406c28ebd4c9fac8e3c0bdf5e4d5b5
OwenRago <owen@rago.net>
2023-10-31 23:52:22 -0400

    Updated customer compare with distance calculation

--- src/Customer/CustomerCompare.js
+++ src/Customer/CustomerCompare.js
@@ -15,6 +15,20 @@ export default function CustomerViewInventory() {
 
     console.log(computer1)
 
+    function calculateShipping(computer)
+    {
+        var radius = 3959 // miles
+        var dLat = (computer.lat-parseFloat(customerGPS[0])) * Math.PI / 180
+        var dLon = (computer.long-parseFloat(customerGPS[1])) * Math.PI / 180
+        var lat1 = parseFloat(customerGPS[0]) * Math.PI / 180
+        var lat2 = computer.lat * Math.PI / 180
+
+        var a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2)
+        var b = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
+        var distance = radius * b
+        return Math.round(distance * 100)/100
+    }
+
     return (
         <div className='CustomerCompare'>
             <div style={{
@@ -42,12 +56,12 @@ export default function CustomerViewInventory() {
                 <div id='inventory' style={customerCompare.inventory}>
                     <div key={computer1.id}>
                         <div style={customerCompare.computer}>
-                            <div style={customerCompare.center}><b>{computer1.id}</b><br /><br /><b>Total Price: ${computer1.price + 1000.23}</b><br /><br />List Price: ${computer1.price}<br />Shopping Cost: ${1000.23}<br />Memory: {computer1.memory}<br />Storage Size: {computer1.storage}<br />Processor: {computer1.processor}<br />Processor Gen: {computer1.processorgen}<br />Graphics: {computer1.graphics}<br />Store: {computer1.store}<br />Shipping: ${1000.23}</div>
+                            <div style={customerCompare.center}><b>{computer1.id}</b><br /><br /><b>Total Price: ${computer1.price + calculateShipping(computer1)}</b><br /><br />List Price: ${computer1.price}<br />Shopping Cost: ${calculateShipping(computer1)}<br />Memory: {computer1.memory}<br />Storage Size: {computer1.storage}<br />Processor: {computer1.processor}<br />Processor Gen: {computer1.processorgen}<br />Graphics: {computer1.graphics}<br />Store: {computer1.store}<br />Shipping: ${1000.23}</div>
                         </div>
                     </div>
                     <div key={computer2.id}>
                         <div style={customerCompare.computer}>
-                            <div style={customerCompare.center}><b>{computer2.id}</b><br /><br /><b>Total Price: ${computer2.price + 1000.23}</b><br /><br />List Price: ${computer2.price}<br />Shopping Cost: ${1000.23}<br />Memory: {computer2.memory}<br />Storage Size: {computer2.storage}<br />Processor: {computer2.processor}<br />Processor Gen: {computer2.processorgen}<br />Graphics: {computer2.graphics}<br />Store: {computer2.store}<br />Shipping: ${1000.23}</div>
+                            <div style={customerCompare.center}><b>{computer2.id}</b><br /><br /><b>Total Price: ${computer2.price + calculateShipping(computer2)}</b><br /><br />List Price: ${computer2.price}<br />Shopping Cost: ${calculateShipping(computer2)}<br />Memory: {computer2.memory}<br />Storage Size: {computer2.storage}<br />Processor: {computer2.processor}<br />Processor Gen: {computer2.processorgen}<br />Graphics: {computer2.graphics}<br />Store: {computer2.store}<br />Shipping: ${1000.23}</div>
                         </div>
                     </div>
                 </div>
